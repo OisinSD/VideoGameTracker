d45e86ee5d253ef47b487d490c114c15140e7e78
YuHang-code-maker <zhoushiisontheway@gmail.com>
2025-03-26 21:22:37 +0000

    Add Footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{ backgroundColor: "#121212" }}
+      className="text-light py-2 mt-auto"
+    >
+      <div className="container text-center">
+        <p className="mb-2">© 2025 Quest Log. All rights reserved.</p>
+
+        <div className="d-flex justify-content-center gap-3 mb-3">
+          <Link to="#" className="text-light text-decoration-none">
+            Privacy
+          </Link>
+          <Link to="#" className="text-light text-decoration-none">
+            Terms
+          </Link>
+          <Link to="#" className="text-light text-decoration-none">
+            About
+          </Link>
+          <Link to="#" className="text-light text-decoration-none">
+            Contact
+          </Link>
+        </div>
+
+        <div className="d-flex justify-content-center gap-4">
+          <a
+            href="https://twitter.com"
+            target="_blank"
+            rel="noreferrer"
+            className="text-light fs-5"
+          >
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noreferrer"
+            className="text-light fs-5"
+          >
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noreferrer"
+            className="text-light fs-5"
+          >
+            <i className="fab fa-facebook"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
