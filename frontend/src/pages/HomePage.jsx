81732fff6a6793ee83dd184468906ddcf0d2e0b6
jonsmacbookair <jonathan.morrisseyto@gmail.com>
2025-04-10 14:01:07 +0100

    Added little Mario running acorss the screen

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -12,6 +12,7 @@ import { signOut } from "firebase/auth";
 import { auth } from "../authentication/firebaseConfig";
 import GameCardDisplay from "../components/GameCardDisplay";
 import GameRecommendationsCarousel from "../components/GameRecommendationsCarousel";
+import PixelRunner from "../components/PixelRunner";
 
 
 const HomePage = () => {
@@ -48,105 +49,88 @@ const HomePage = () => {
   };
 
   return (
-    <div className="home-page">
-      <header className="position-relative">
-        <div className="banner-wrapper">
-          {/* <img
-            src="/VideoGameCharacters-JuegoStudioBackground.png"
-            alt="Banner"
-            className="banner-image"
-            style={{height:"30vh"}}
-          /> */}
 
-          <div className="banner">
-            {/* Profile button */}
-            <div className="position-absolute top-0 start-0 m-3">
-              <Button
-                onClick={() => setShowProfileModal(true)}
-                className="btn btn-lg"
-                style={{
-                  background: "linear-gradient(90deg, #7f57f5, #e157f5)",
-                  border: "none",
-                }}
-              >
-                Profile
-              </Button>
-            </div>
+      <div className="home-page">
+          <header className="position-relative">
+              <div className="banner-wrapper">
+                  <div className="banner">
+                      {/* Profile button */}
+                      <div className="position-absolute top-0 start-0 m-3">
+                          <Button
+                              onClick={() => setShowProfileModal(true)}
+                              className="btn btn-lg"
+                              style={{
+                                  background: "linear-gradient(90deg, #7f57f5, #e157f5)",
+                                  border: "none",
+                              }}
+                          >
+                              Profile
+                          </Button>
+                      </div>
 
-            {/* Top-right corner: Add Game + Logout */}
-              <div className="position-absolute top-0 end-0 m-3 d-flex gap-2">
-                  {/*}
-              <Button
-                variant="primary"
-                onClick={() => setShowAddModal(true)} // 👈 open AddGame instead
-                className="btn btn-lg w-30"
-                style={{
-                  background: "linear-gradient(90deg, #7f57f5, #e157f5)",
-                  border: "none",
-                }}
-              >
-                <Plus size={20} /> Add Game
-              </Button>
-              */}
+                      {/* Top-right corner: Logout */}
+                      <div className="position-absolute top-0 end-0 m-3 d-flex gap-2">
+                          <Button
+                              className="btn btn-lg"
+                              style={{
+                                  background: "linear-gradient(90deg, #7f57f5, #e157f5)",
+                                  border: "none",
+                              }}
+                              onClick={() => signOut(auth)}
+                          >
+                              Logout
+                          </Button>
+                      </div>
+                  </div>
 
+                  {/* ⬇️ Add PixelRunner here */}
+                  <PixelRunner/>
 
-                  <Button
-                      className="btn btn-lg"
-                      style={{
-                          background: "linear-gradient(90deg, #7f57f5, #e157f5)",
-                          border: "none",
-                      }}
-                      onClick={() => signOut(auth)}
-                  >
-                      Logout
-                  </Button>
+                  {/* Search bar only */}
+                  <div className="search-bar-container">
+                      <NewSearchBar
+                          setResults={setResults}
+                          results={results}
+                          triggerGameInfo={handleGameSelectFromSearch}
+                      />
+
+                      <SearchResults
+                          results={results}
+                          onSelectGame={handleGameSelectFromSearch}
+                      />
+                  </div>
               </div>
-          </div>
-
-            {/* Search bar only */}
-            <div className="search-bar-container">
-                <NewSearchBar
-                    setResults={setResults}
-                    results={results}
-              triggerGameInfo={handleGameSelectFromSearch}
-            />
-
-            <SearchResults
-              results={results}
-              onSelectGame={handleGameSelectFromSearch}
-            />
-          </div>
-        </div>
-      </header>
-
-        <GameRecommendationsCarousel />
+          </header>
 
 
-        <GameCardDisplay refreshTrigger={refreshGames} />
+          <GameRecommendationsCarousel/>
 
-      {/* Modals */}
-      <ProfilePage
-        show={showProfileModal}
-        handleClose={() => setShowProfileModal(false)}
-        refreshTrigger={refreshGames}
-      />
-      <AddGame
-        show={showAddModal}
-        handleClose={() => setShowAddModal(false)}
-        handleAddData={handleAddData}
-        game={selectedGame}
-        extraData={addGameData}
-        setRefreshGames={setRefreshGames}
-      />
-      <GameInfo
-        show={showInfoModal}
-        handleClose={() => setShowInfoModal(false)}
-        handleAddData={handleAddData}
-        triggerAddGame={triggerAddGame}
-        game={selectedGame}
-        setRefreshGames={setRefreshGames}
-      />
-    </div>
+
+          <GameCardDisplay refreshTrigger={refreshGames}/>
+
+          {/* Modals */}
+          <ProfilePage
+              show={showProfileModal}
+              handleClose={() => setShowProfileModal(false)}
+              refreshTrigger={refreshGames}
+          />
+          <AddGame
+              show={showAddModal}
+              handleClose={() => setShowAddModal(false)}
+              handleAddData={handleAddData}
+              game={selectedGame}
+              extraData={addGameData}
+              setRefreshGames={setRefreshGames}
+          />
+          <GameInfo
+              show={showInfoModal}
+              handleClose={() => setShowInfoModal(false)}
+              handleAddData={handleAddData}
+              triggerAddGame={triggerAddGame}
+              game={selectedGame}
+              setRefreshGames={setRefreshGames}
+          />
+      </div>
   );
 };
 
